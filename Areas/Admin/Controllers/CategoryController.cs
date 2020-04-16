4ad2002578e79187013b6f8975ee6a032d03381e
elaimaz <fariawesley4@gmail.com>
2020-04-16 12:07:53 -0300

    category Index

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
+        private readonly IUnityOfWork _unitOfWork;
+
+        public CategoryController(IUnityOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj});
+        }
+
+        #endregion
     }
 }
