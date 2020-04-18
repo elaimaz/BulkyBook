dce10c34cec96d943284474552fb571a1d4e7ade
elaimaz <fariawesley4@gmail.com>
2020-04-18 15:53:42 -0300

    Product Controller

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using BulkyBook.DataAccess.Data.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnityOfWork _unitOfWork;
+        //IWebHostEnvironment is needed to upload images
+        private readonly IWebHostEnvironment _hostEnviroment;
+
+        public ProductController(IUnityOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnviroment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int ?id)
+        {
+            Product product = new Product();
+            if (id == null)
+            {
+                //This is for create
+                return View(product);
+            }
+            //This is for Edit
+            product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                if (product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(product);
+                }
+                _unitOfWork.Save();
+            }
+            return View(product);
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Product.GetAll();
+            return Json(new { data = allObj});
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Product.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error While Deleting"});
+            }
+            _unitOfWork.Product.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Product Deleted"});
+        }
+        
+        #endregion
+    }
+}
