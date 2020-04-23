5e23df4b05cbe81ec55301ee2ddfc36153eedc3e
elaimaz <fariawesley4@gmail.com>
2020-04-23 16:24:19 -0300

    User lock and unlock

--- wwwroot/js/user.js
+++ wwwroot/js/user.js
@@ -14,7 +14,34 @@ function loadDataTable() {
             { "data": "email", "width": "15%" },
             { "data": "phoneNumber", "width": "15%" },
             { "data": "company.name", "width": "15%" },
-            { "data": "role", "width": "15%" }
+            { "data": "role", "width": "15%" },
+            {
+                "data": {
+                    id: "id", lockoutEnd: "lockoutEnd"
+                },
+                "render": function (data) {
+                    var today = new Date().getTime()
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        //User is currently locked
+                        return `
+                            <div class="text-centered">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-lock-open"></i> Unlock
+                                </a>
+                            </div>
+                        `;
+                    } else {
+                        return `
+                            <div class="text-centered">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-lock"></i> Lock
+                                </a>
+                            </div>
+                        `;
+                    }
+                }, "width": "25%"
+            }
         ]
     });
 }
