63637c39e9a104ffc9aae8a72019a5f6f66cc489
elaimaz <fariawesley4@gmail.com>
2020-04-26 16:19:05 -0300

    Shopping Cart Index Action Get

---
+++ Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,52 @@
+﻿using BulkyBook.DataAccess.Data.Repository.IRepository;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyBook.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IUnityOfWork _unitOfWork;
+        private readonly IEmailSender _emailSender;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ShoppingCartVM shoppingCartVM { get; set; }
+
+        public CartController(IUnityOfWork unitOfWork, IEmailSender emailSender, UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _emailSender = emailSender;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            shoppingCartVM = new ShoppingCartVM()
+            {
+                OrderHeader = new Models.OrderHeader(),
+                ListCart = _unitOfWork.ShopingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties:"Product")
+            };
+            shoppingCartVM.OrderHeader.orderTotal = 0;
+            shoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claim.Value, includeProperties:"Company");
+
+            foreach (var list in shoppingCartVM.ListCart)
+            {
+                list.Price = SD.GetPriceBasedOnQuantity(list.Count, list.Product.Price, list.Product.Price50, list.Product.Price100);
+                shoppingCartVM.OrderHeader.orderTotal += (list.Price * list.Count);
+                list.Product.Description = SD.ConvertToRawHtml(list.Product.Description);
+                if (list.Product.Description.Length > 100)
+                {
+                    list.Product.Description = list.Product.Description.Substring(0, 99) + "...";
+                }
+            }
+            return View(shoppingCartVM);
+        }
+    }
+}
