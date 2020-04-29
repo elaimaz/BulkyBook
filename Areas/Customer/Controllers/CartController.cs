5991e71966c1ab7e31f2165850f4b3f3a97b9921
elaimaz <fariawesley4@gmail.com>
2020-04-29 17:23:21 -0300

    Validation for Summary

--- Areas/Customer/Controllers/CartController.cs
+++ Areas/Customer/Controllers/CartController.cs
@@ -200,7 +200,10 @@ namespace BulkyBook.Areas.Customer.Controllers
 
             if (stripeToken == null)
             {
-
+                //order will be created for delayed payment for authorized company
+                ShoppingCartVM.OrderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusApproved;
             }
             else
             {
