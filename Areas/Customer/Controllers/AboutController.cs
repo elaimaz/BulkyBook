9dba0296f08dfa3f189915a7eceff19ae62b00f6
elaimaz <fariawesley4@gmail.com>
2020-05-03 17:20:27 -0300

    Created About Pages and updated packeges

---
+++ Areas/Customer/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Bugs()
+        {
+            return View();
+        }
+    }
+}
